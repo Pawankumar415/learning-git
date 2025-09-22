c8ee54098a62c520de6710f3c8a4470141e68279
pawankumar <pawankounder415@gmail.com>
2025-09-22 11:17:08 +0530

    added multiplication table function

--- demo.py
+++ demo.py
@@ -6,5 +6,12 @@ print("this is from feature1 branch")
 def greet_name(name):
     return f"welcome {name} from branching demo"
 
-
 greet_name("pawan kumar")
+
+
+def table(number):
+    for i in range(1,11):
+        return f"{number} * {i} = {number*i}"
+    
+
+table(7)
