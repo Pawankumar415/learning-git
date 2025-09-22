ce359b32b24a6748d2bba48c6c6e9d6ebbdfc0a7
pawankumar <pawankounder415@gmail.com>
2025-09-22 15:02:53 +0530

    added odd or even function in feature1 branch

--- demo.py
+++ demo.py
@@ -44,3 +44,10 @@ print("second dummy print")
 
 print("this is dummy code")
 
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(f"{num} is an even number.")
+else:
+    print(f"{num} is an odd number.")
